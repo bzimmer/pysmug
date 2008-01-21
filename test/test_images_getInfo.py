e11730eae42c596e33f4cc5f3c073d2592bff754
Brian Zimmer <bzimmer@ziclix.com>
2008-01-21 10:10:05 -0800

    added tests for password and anon logins

--- test/test_images_getInfo.py
+++ test/test_images_getInfo.py
@@ -1,45 +1,61 @@
 
 import optparse
 from pysmug import SmugMug
+from getpass import getpass
 
-APIKey = "1XhqbbxNfSygsmVReGQ8nek8D2Dz8F61"
+_APIKey = "1XhqbbxNfSygsmVReGQ8nek8D2Dz8F61"
 
-def test(albumId=630992):
-  # login
-  m = SmugMug().login_anonymously(APIKey=APIKey)
-  b = m.batch()
-  
-  album = m.albums_getInfo(AlbumID=albumId)
-  print
-  print "Album:", album["Album"]["Title"]
+class Test:
+  def __init__(self, APIKey, EmailAddress=None, Password=None):
+    self.m = SmugMug()
+    if EmailAddress and Password:
+      self.m.login_withPassword(APIKey=APIKey,
+        EmailAddress=EmailAddress, Password=Password)
+    else:
+      self.m.login_anonymously(APIKey=APIKey)
 
-  images = m.images_get(AlbumID=albumId)
-  for image in images["Images"]:
-    # add each image to the batch
-    b.images_getInfo(ImageID=image["id"], Heavy=1)
+  def test(self, albumId):
+    m, b = self.m, self.m.batch()
+    album = m.albums_getInfo(AlbumID=albumId)
+    print
+    print "Album:", album["Album"]["Title"],
 
-  count = 0
-  max_time = 0.0
-  total_time = 0.0
+    images = m.images_get(AlbumID=albumId)
+    for image in images["Images"]:
+      # add each image to the batch
+      b.images_getInfo(ImageID=image["id"], Heavy=1)
+    
+    count = len(b)
+    print "(%d photos)" % (len(b))
 
-  # execute the batch request
-  for (params, value) in b():
-    count += 1
-    stats = value["Statistics"]
-    max_time = max(max_time, stats["total-time"])
-    total_time += stats["total-time"]
+    max_time = 0.0
+    total_time = 0.0
 
-  # print some stats
-  print "Number of photos:", count
-  print "Synchronously:", total_time
-  print "Asynchronously:", max_time
+    # execute the batch request
+    for (params, value) in b():
+      stats = value["Statistics"]
+      max_time = max(max_time, stats["total-time"])
+      total_time += stats["total-time"]
 
-p = optparse.OptionParser()
-opts, args = p.parse_args()
-if not args:
-  args = [630992] # Street Photos from Moon River Photography
+    # print some stats
+    def seconds(g, t):
+      return "%s: %0.2f seconds" % (g, t)
+    print seconds("Synchronously        ", total_time)
+    print seconds("Asynchronously       ", max_time)
+    print seconds("Average response time", total_time/count)
 
-for arg in args:
-  test(int(arg))
-raise SystemExit()
+if __name__ == "__main__":
+  p = optparse.OptionParser()
+  p.add_option("-a", "--apikey", default=_APIKey, action="store")
+  p.add_option("-e", "--email", action="store")
+  opts, args = p.parse_args()
+
+  if not args:
+    args = [630992] # Street Photos from Moon River Photography
+
+  password = getpass() if opts.email else None
+  test = Test(opts.apikey, opts.email, password)
+  for arg in args:
+    test.test(arg)
+  raise SystemExit()
 
