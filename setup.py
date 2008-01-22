3bb3f90d5decabb4950cd7cab55d0e7c1d1643c3
Brian Zimmer <bzimmer@ziclix.com>
2008-01-22 07:48:27 -0800

    added additional meta-data to setup.py

--- setup.py
+++ setup.py
@@ -8,10 +8,18 @@ from distutils.core import setup
 setup(
   name = "pysmug",
   version = pysmug.__version__,
+  description = "A high-performance python client for the SmugMug API.",
   author = "Brian Zimmer",
   author_email = "bzimmer@ziclix.com",
   url = "http://code.google.com/p/pysmug",
   packages = ['pysmug'],
-  license = "Python",
+  platforms = ['any'],
+  license = "MIT License",
+  classifiers = [
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python',
+    'Topic :: Software Development :: Libraries :: Python Modules',
+  ],
 )
 
