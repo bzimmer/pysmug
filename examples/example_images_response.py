9975def88b82cdaea7997ff0acc107dc35663351
Brian Zimmer <bzimmer@ziclix.com>
2008-01-24 07:10:55 -0800

    initial

---
+++ examples/example_images_response.py
@@ -0,0 +1,26 @@
+
+import smexample
+
+class Example(smexample.Example):
+  
+  def example(self, albumId):
+    m, b = self.m, self.m.batch()
+    album = m.albums_getInfo(AlbumID=albumId)
+    print
+    print "Album:", album["Album"]["Title"],
+
+    images = m.images_get(AlbumID=albumId)
+    for image in images["Images"]:
+      # add each image to the batch
+      b.images_getInfo(ImageID=image["id"], Heavy=1)
+    
+    count = len(b)
+    print "(%d photos)" % (len(b))
+
+    # execute the batch request
+    for (params, value) in b():
+      print params, value
+
+if __name__ == "__main__":
+  smexample.main(Example)
+
